ac6e33c4359551d924c072c5c04c23b6ee1eae07
Saurab-18 <106486115+Saurab-18@users.noreply.github.com>
2023-08-23 15:00:23 +0530

    Complete Files

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (apiPath, queryTerm="") => {
+    const [data, setData] = useState([]);
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=7c9f80882e52453993ea7849df5afa1b&query=${queryTerm}`
+
+    useEffect(() => {
+        async function fetchMovies(){
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json.results);
+        }
+        fetchMovies();
+      }, [url])
+
+  return { data }
+}
